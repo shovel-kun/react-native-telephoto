f4a19fc9c624c4283b297ff71d8b714867cd693f
Ebisuzawa Kurumi <ebisuzawakurumi@proton.me>
2025-04-22 01:23:15 +0800

    fix: try not to crash in rn-screens

--- android/src/main/java/com/telephoto/TelephotoView.kt
+++ android/src/main/java/com/telephoto/TelephotoView.kt
@@ -1,8 +1,11 @@
 package com.telephoto
 
 import android.annotation.SuppressLint
+import android.graphics.Canvas
 import android.util.Log
+import android.view.View
 import android.widget.FrameLayout
+import androidx.annotation.UiThread
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,11 +15,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.InternalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.geometry.Offset
 import com.margelo.nitro.telephoto.Offset as NitroOffset
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.facebook.react.uimanager.BackgroundStyleApplicator
 import com.facebook.react.uimanager.PixelUtil.dpToPx
 import com.facebook.react.uimanager.PixelUtil.pxToDp
 import com.facebook.react.uimanager.ThemedReactContext
@@ -124,5 +130,79 @@ class TelephotoView(context: ThemedReactContext) : FrameLayout(context) {
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         zoomChannel.close()
+    }
+
+
+    // Code taken from
+    // https://github.com/expo/expo/blob/26cbac0e18f851fe07617a34f862416f756fd74c/packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/ExpoView.kt#L4
+    // https://github.com/expo/expo/blob/26cbac0e18f851fe07617a34f862416f756fd74c/packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/ExpoComposeView.kt#L14
+
+    open val shouldUseAndroidLayout: Boolean = true
+
+    /**
+     * Manually trigger measure and layout.
+     * If [shouldUseAndroidLayout] is set to `true`, this method will be called automatically after [requestLayout].
+     */
+    @UiThread
+    fun measureAndLayout() {
+        measure(
+            MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
+            MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+        )
+        layout(left, top, right, bottom)
+    }
+
+    override fun requestLayout() {
+        super.requestLayout()
+        if (shouldUseAndroidLayout) {
+            // We need to force measure and layout, because React Native doesn't do it for us.
+            post(Runnable { measureAndLayout() })
+        }
+    }
+
+    open fun clipToPaddingBox(canvas: Canvas) {
+        // When the border radius is set, we need to clip the content to the padding box.
+        // This is because the border radius is applied to the background drawable, not the view itself.
+        // It is the same behavior as in React Native.
+        if (!clipToPadding) {
+            return
+        }
+        BackgroundStyleApplicator.clipToPaddingBox(this, canvas)
+    }
+
+    override fun dispatchDraw(canvas: Canvas) {
+        clipToPaddingBox(canvas)
+        super.dispatchDraw(canvas)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // In case of issues there's an alternative solution in previous commits at https://github.com/expo/expo/pull/33759
+        if (!isAttachedToWindow) {
+            setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
+            return
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    @OptIn(InternalComposeUiApi::class)
+    val layout by lazy {
+        ComposeView(context).also {
+            it.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+            it.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            addView(it)
+            it.addOnAttachStateChangeListener(object : OnAttachStateChangeListener {
+                override fun onViewAttachedToWindow(v: View) {
+                    it.disposeComposition()
+                }
+
+                override fun onViewDetachedFromWindow(v: View) = Unit
+            })
+        }
+    }
+
+    fun setContent(
+        content: @Composable () -> Unit
+    ) {
+        layout.setContent { content() }
     }
 }
